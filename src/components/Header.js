237c866bcb3b5a37c199efc2f7bce605793a586a
Harsh8196 <harshmashruwala8196@gmail.com>
2023-08-08 08:47:57 +0530

    change name

--- src/components/Header.js
+++ src/components/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">WrapStepper</Link>
+                <Link className="navbar-brand" to="/">WarpStepper</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
